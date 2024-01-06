47fec847b700b7e37911001dc44a0cd6f5316a3e
Santosh Saha <138041031+San2021331091@users.noreply.github.com>
2024-01-06 10:45:22 +0600

    using random class to generate enemies

--- Utils.h
+++ Utils.h
@@ -1,25 +1,15 @@
-#ifndef CADILLACS_AND_DINOSAUR_GAME_UTILS_H
-#define CADILLACS_AND_DINOSAUR_GAME_UTILS_H
 
+#ifndef CADILLACS_AND_DINOSAURS_GAME_UTILS_H
+#define CADILLACS_AND_DINOSAURS_GAME_UTILS_H
 #include "Game.h"
 
 class Utils {
 
-
-
 public:
-
-
-    static void vector3d_zero(vector3d_t * vec);
+    static void vector3d_zero(vector3d_t* vec);
+    static float get_RandF(float a, float b);
     static int get_rand(int min, int max);
-    static float get_Random(float a, float b);
-    static Font create_font(const string &name, unsigned int size);
-
-
-
-
-
-
+    static HFONT create_font(const wchar_t* name, int size);
 
 
 };
