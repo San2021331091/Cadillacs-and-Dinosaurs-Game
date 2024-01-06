d0167d62cce5e60807b159b0a31163f867ad1a78
Santosh Saha <138041031+San2021331091@users.noreply.github.com>
2024-01-06 10:41:02 +0600

    player control

--- Player.h
+++ Player.h
@@ -1,19 +1,20 @@
-#ifndef CADILLACS_AND_DINOSAUR_GAME_PLAYER_H
-#define CADILLACS_AND_DINOSAUR_GAME_PLAYER_H
+#ifndef CADILLACS_AND_DINOSAURS_GAME_PLAYER_H
+#define CADILLACS_AND_DINOSAURS_GAME_PLAYER_H
 
 #include "Game.h"
 
+
+
+
 class Player {
 
-
 public:
-    static player_t * player_new();
+    static player_t* player_new();
     static void player_delete(player_t* player);
-    static void player_draw(vector<player_t>* player);
+    static void player_draw(player_t *player,HDC hdc);
+    static void player_update(player_t* player, float dt);
     static void player_update_state(player_t* player, float dt);
-    static void player_update(vector<player_t>* pl, float dt);
     static void player_set_state(player_t* player, int state);
-
 
 
 };
