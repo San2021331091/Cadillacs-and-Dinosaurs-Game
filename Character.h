c6ac86c7148ed23dcaa13ef84bd82d7059737c20
Santosh Saha <138041031+San2021331091@users.noreply.github.com>
2024-01-06 10:35:59 +0600

    game design

--- Character.h
+++ Character.h
@@ -1,28 +1,15 @@
-#ifndef CADILLACS_AND_DINOSAUR_GAME_CHARACTER_H
-#define CADILLACS_AND_DINOSAUR_GAME_CHARACTER_H
 
+#ifndef CADILLACS_AND_DINOSAURS_GAME_CHARACTER_H
+#define CADILLACS_AND_DINOSAURS_GAME_CHARACTER_H
 #include "Game.h"
-
 
 class Character {
 
+
 public:
-
-    static void character_get_center(character_t* character, vector3d_t* center);
     static void player_calculate_hit_boxes(player_t* player);
-    static bool rect_collision(FloatRect& rect1, FloatRect& rect2);
-    static void enemy_calculate_hit_boxes(enemy_t* enemy);
-
-
-
-
-
-
-
-
-
-
-
+    static void enemy_calculate_hit_boxes(enemy_t* enemy) ;
+    static void character_get_center(character_t* character, vector3d_t* center);
 };
 
 
