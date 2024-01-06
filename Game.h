e6faaaa296d5d0773e321e97f131b9c3caecf37c
Santosh Saha <138041031+San2021331091@users.noreply.github.com>
2024-01-06 10:34:07 +0600

    game loop design

--- Game.h
+++ Game.h
@@ -1,15 +1,14 @@
-#ifndef CADILLACS_AND_DINOSAUR_GAME_GAME_H
+#ifndef CADILLACS_AND_DINOSAURS_GAME_GAME_H
+#define CADILLACS_AND_DINOSAURS_GAME_GAME_H
 
-#define CADILLACS_AND_DINOSAUR_GAME_GAME_H
-
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include <windows.h>
+#include <winuser.h>
 #include<SFML/Graphics.hpp>
-
-
 
 using namespace std;
 using namespace sf;
+
 
 
 #define CHARACTER_STATE_IDLE 1
@@ -23,8 +22,10 @@ using namespace sf;
 #define CHARACTER_STATE_JUMP 9
 #define CHARACTER_STATE_RANGE_ATTACK 10
 #define CHARACTER_STATE_WIN 11
+
 #define CHARACTER_DIRECTION_RIGHT 1
 #define CHARACTER_DIRECTION_LEFT -1
+
 #define GAME_STATE_MENU 0
 #define GAME_STATE_INTRO 1
 #define GAME_STATE_CUTSCENE 2
@@ -34,23 +35,16 @@ using namespace sf;
 #define GAME_STATE_WIN 6
 
 
-class vector3d_t{
 
+class vector3d_t {
 public:
-
-    float x,y,z;
-
-
+    float x, y, z;
 };
 
 
-
-class animation_t{
-
+class animation_t {
 public:
-
-
-    string name; //using string class
+    wchar_t name[256];
     int frame_start;
     int frame_end;
     float speed;
@@ -58,22 +52,14 @@ public:
     int reverse_loop;
     int flag;
     int animation_ended;
-
-
-
-
-
-
 };
 
-class sprite_t{
 
+class sprite_t {
 public:
-
-
-    string file_name; //using string class
-    Image image;
-    Color color_mask;
+    wchar_t file_name[256];
+    HBITMAP hbitmap;
+    COLORREF color_mask;
     int width;
     int height;
     float scale;
@@ -82,27 +68,22 @@ public:
     int frame;
     int frame_x;
     int frame_y;
-    bool flag;
-    vector<animation_t>animations;
+    int flag;
+    animation_t* animations;
     int animation_max;
     int animation_count;
     int animation_current;
     double animation_time;
-
-
-
-
 };
 
-class game_object_t{
 
+
+
+class game_object_t {
 public:
-
     vector3d_t position;
-
-
-
 };
+
 
 #define BBOX_MAIN 0
 #define BBOX_HEAD 1
@@ -110,71 +91,69 @@ public:
 #define BBOX_FIST 3
 #define BBOX_LEGS 4
 
-
-class character_t{
-
+class character_t {
 public:
-
-    string name;
-    vector3d_t position,velocity;
+    wchar_t name[256];
+     vector3d_t position;
+     vector3d_t velocity;
     int on_ground;
     int direction;
     int prev_state;
     int state;
     int state_on_enter;
-    vector<sprite_t>sprite;
+    sprite_t* sprite;
     float health;
-    FloatRect * hit_boxes;
+    RECT* hit_boxes;
     int hit_box_count;
     int visible;
-    float jump_speed;
-
 };
 
 #define ENEMY_TYPE_FERRIS 1
 #define ENEMY_TYPE_GNEISS 2
 #define ENEMY_TYPE_BUTCHER 3
+#define ENEMY_TYPE_DINOSAUR 4
 
-
-class enemy_t : public character_t,public sprite_t{
-
+class enemy_t {
 public:
-
+    character_t base;
     float movement_speed;
     double animation_time;
-    float hit_reset,hit_recover_time,time_to_attack;
-    int being_hit,engaging,marked_for_delete;
-    vector<animation_t>walk;
+    float hit_reset;
+    float hit_recover_time;
+    float time_to_attack;
+    int being_hit;
+    int engaging;
+    int marked_for_delete;
+    animation_t* walk;
     float walk_time;
     int walk_target_idx;
-    float target_x,target_z,death_blink_time;
+    float target_x;
+    float target_z;
+    float death_blink_time;
     int type;
-    float damage_takes,hit_streak;
-
-
-
+    float damage_takes;
+    float hit_streak;
 };
 
-class player_t : public character_t{
 
+
+class player_t {
 public:
-
-    float movement_speed, hit_recover_time;
+    character_t base;
+    float movement_speed;
+    float hit_recover_time;
     int score;
-    float hit_reset,hit_streak;
-    vector<animation_t>anim_upper_cut;
-
-
+    float hit_reset;
+    float hit_streak;
+    animation_t* anim_upper_cut;
 };
 
-
-class draw_t : public vector3d_t{
-
+class draw_t {
 public:
-
-    Image image;
+    HBITMAP hbitmap;
     int flip;
-    Color color_mask;
+    COLORREF color_mask;
+    vector3d_t position;
     int width;
     int height;
     int src_x;
@@ -182,67 +161,79 @@ public:
     int src_width;
     int src_height;
     int draw_top;
-
 };
 
-class effect_t : public vector3d_t{
 
+class effect_t {
 public:
-
-
-    vector<sprite_t>sprite;
+    sprite_t* sprite;
+    vector3d_t pos;
     int draw_punch;
-    animation_t *anim;
-
-
-
+    animation_t* anim;
 };
 
-class game_t{
-
+class game_t {
 public:
-
-
-    RenderWindow window;
-    Image dbl_buffer;
-    string title;
-    int height,width,top_margin;
-    Font font,font_big;
+    HWND hwnd;
+    wchar_t title[256];
+    int height;
+    int width;
+    int top_margin;
+    HBITMAP dbl_buffer;
+    HFONT font;
+    HFONT font_big;
     double time_elapsed;
-    float view_x,view_x_far,rect_width,ground_y,gravity;
+
+    float view_x;
+    float view_x_far;
+
+    float rect_width;
+
+    float ground_y;
+    float gravity;
+
     int state;
-    effect_t fx_punch,fx_blood;
-    string keyboard_state;
-    vector<player_t>player;
-    vector<enemy_t>enemies;
-    int enemy_max,enemy_count;
-    Image img_bg,img_bg_far,img_avatar_player,img_text;
-    Image * img_level_layers;
-    vector<sprite_t> level_fire;
-    float fx_smack_x,fx_smack_y,fx_smack_time;
-    draw_t *draw_list;
+    char keyboard_state[256];
+
+    player_t* player;
+
+    enemy_t* enemies;
+    int enemy_max;
+    int enemy_count;
+
+    HGDIOBJ bmp_bg;
+    HGDIOBJ bmp_bg_far;
+    vector<HGDIOBJ>bmp_level_layers;
+
+    sprite_t* level_fire;
+
+    HGDIOBJ bmp_avatar_player;
+
+    effect_t fx_punch;
+    effect_t fx_blood;
+    HGDIOBJ bmp_text;
+    float fx_smack_x;
+    float fx_smack_y;
+    float fx_smack_time;
+
+    // things to draw on screen
+    draw_t* draw_list;
     int draw_max;
     int draw_count;
+
     float max_z;
     float min_z;
     float max_view_x;
     float time_text_blink;
+
     float intro_time;
     int spawn_trigger;
-
-
-
 };
 
 
 
-extern game_t *game;
+extern game_t* game;
 
 
 #endif
-
-
-
-
-
 
