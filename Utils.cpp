bc5b39329d5324e405619ed2a416c1cac758198d
Santosh Saha <138041031+San2021331091@users.noreply.github.com>
2024-02-10 22:17:52 +0600

    xor operation

--- Utils.cpp
+++ Utils.cpp
@@ -1,38 +1,33 @@
 
-#include "Utils.h"
+#ifndef CADILLACS_AND_DINOSAURS_GAME_UTILS_H
+#define CADILLACS_AND_DINOSAURS_GAME_UTILS_H
+#include "Game.h"
+
+class Utils {
+
+public:
+    static void vector3d_zero(vector3d_t* vec);
+    static float get_RandF(float a, float b);
+    static int get_rand(int min, int max);
+    static HFONT create_font(const wchar_t* name, int size);
 
 
-void Utils :: vector3d_zero(struct vector3d_t* vec) {
-    vec->x = 0;
-    vec->y = 0;
-    vec->z = 0;
-}
+};
 
-int Utils :: get_rand(int min, int max) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> distribution(0, max - min);
+class XorShift32 {
+private:
+    uint32_t state;
 
-    return distribution(gen) + min;
-}
+public:
+    XorShift32(uint32_t seed) : state(seed) {}
 
-float Utils :: get_RandF(float a, float b) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<float> distribution(0.0f, 1.0f);
+    float nextFloat(float min, float max);
+};
 
-    float random = distribution(gen);
-    float diff = b - a;
-    float r = random * diff;
 
-    return a + r;
-}
 
-HFONT Utils ::  create_font(const wchar_t* name, int size) {
-    HDC hDC = GetDC(HWND_DESKTOP);
-    int hight = -MulDiv(size, GetDeviceCaps(hDC, LOGPIXELSY), 72);
-    ReleaseDC(HWND_DESKTOP, hDC);
-    HFONT font = CreateFontW(hight, 0, 0, 0, FW_BOLD, 0, 0, 0,
-                             DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, name);
-    return font;
-}
+
+
+
+
+#endif
