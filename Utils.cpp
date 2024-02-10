734a58d313ddf122d6ee2839093042c75425f461
Santosh Saha <138041031+San2021331091@users.noreply.github.com>
2024-02-10 22:23:56 +0600

    game loop design

--- Utils.cpp
+++ Utils.cpp
@@ -1,33 +1,48 @@
 
-#ifndef CADILLACS_AND_DINOSAURS_GAME_UTILS_H
-#define CADILLACS_AND_DINOSAURS_GAME_UTILS_H
-#include "Game.h"
-
-class Utils {
-
-public:
-    static void vector3d_zero(vector3d_t* vec);
-    static float get_RandF(float a, float b);
-    static int get_rand(int min, int max);
-    static HFONT create_font(const wchar_t* name, int size);
+#include "Utils.h"
 
 
-};
+void Utils :: vector3d_zero(vector3d_t* vec) {
+    vec->x = 0;
+    vec->y = 0;
+    vec->z = 0;
+}
 
-class XorShift32 {
-private:
-    uint32_t state;
+int Utils :: get_rand(int min, int max) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> distribution(0, max - min);
 
-public:
-    XorShift32(uint32_t seed) : state(seed) {}
+    return distribution(gen) + min;
+}
 
-    float nextFloat(float min, float max);
-};
+float Utils :: get_RandF(float a, float b) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<float> distribution(0.0f, 1.0f);
 
+    float random = distribution(gen);
+    float diff = b - a;
+    float r = random * diff;
 
+    return a + r;
+}
 
+HFONT Utils ::  create_font(const wchar_t* name, int size) {
+    HDC hDC = GetDC(HWND_DESKTOP);
+    int height = -MulDiv(size, GetDeviceCaps(hDC, LOGPIXELSY), 72);
+    ReleaseDC(HWND_DESKTOP, hDC);
+    HFONT font = CreateFontW(height, 0, 0, 0, FW_BOLD, 0, 0, 0,
+                             DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, name);
+    return font;
+}
 
-
-
-
-#endif
+float XorShift32 :: nextFloat(float min, float max) {
+        uint32_t x = state;
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        state = x;
+        float normalized = static_cast<float>(x) / static_cast<float>(UINT32_MAX);
+        return min + normalized * (max - min);
+    }
